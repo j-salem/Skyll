1bcb58db267056e270b1a8f5d8ffeb44a4546fa1
j-salem <john.d.salem@gmail.com>
2015-09-16 18:45:26 -0700

    More Matrix math

--- Skyll/Skyll/Geometry.fs
+++ Skyll/Skyll/Geometry.fs
@@ -29,6 +29,7 @@ type Vec2D (x0 : float, y0 : float) =
         Vec2D(-a.X, -a.Y)
     override this.ToString() = 
         "{" + this.X.ToString() + ", " + this.Y.ToString() + "}"
+    new() = Vec2D(0.0, 0.0)
 
 type Vec3D(x0 : float, y0 : float, z0 : float) =
     let mutable x, y, z = x0, y0, z0
@@ -61,6 +62,7 @@ type Vec3D(x0 : float, y0 : float, z0 : float) =
         Vec3D(-a.X, -a.Y, -a.Z)
     override this.ToString() = 
         "{" + this.X.ToString() + ", " + this.Y.ToString() + ", " + this.Z.ToString() + "}"
+    new() = Vec3D(0.0, 0.0, 0.0)
 
 type Point2D(x0 : float, y0 : float) =
     let mutable x, y = x0, y0
@@ -74,6 +76,7 @@ type Point2D(x0 : float, y0 : float) =
         Vec2D(a.X - this.X, a.Y - this.Y)
     override this.ToString() =
         "{" + this.X.ToString() + ", " + this.Y.ToString() + "}"
+    new() = Point2D(0.0, 0.0)
 
 type Point3D(x0 : float, y0 : float, z0 : float) =
     let mutable x, y, z = x0, y0, z0
@@ -88,8 +91,124 @@ type Point3D(x0 : float, y0 : float, z0 : float) =
         Vec3D(a.X - this.X, a.Y - this.Y, a.Z - this.Z)
     override this.ToString() =
         "{" + this.X.ToString() + ", " + this.Y.ToString() + ", " + this.Z.ToString() + "}"
+    new() = Point3D(0.0, 0.0, 0.0)
+
+type Matrix(m0 : int, n0 : int) =
+    let n, m = n0, m0
+    let mutable values = [| for i in 1..m -> [| for j in 1..n -> 0.0 |] |]
+
+    member this.Item
+        with get(i, j) = 
+            assert(i < m && j < n)
+            values.[i].[j]
+        and set (i, j) value = 
+            assert(i < m && j < n)
+            values.[i].[j] <- value
+    member this.Item
+        with get(i) = 
+            assert(i < m)
+            values.[i]
+        and set (i) value = 
+            assert(i < m)
+            values.[i] <- value
+    member this.M with get() : int = m
+    member this.N with get() : int = n
+    member this.ScalarMult(s) =
+        let newMat = Matrix(m,n)
+        for i = 0 to m - 1 do
+            for j = 0 to n - 1 do
+                newMat.[i,j] <- this.[i,j] * s
+        newMat
+    member this.Transpose() =
+        let newMat = Matrix(n,m)
+        for i = 0 to m - 1 do
+            for j = 0 to n - 1 do
+                newMat.[j,i] <- this.[i,j]
+        newMat
+    member this.Trace() =
+        assert(m = n) // possibly break this out into a subclass for square matrices
+        let mutable trace = 0.0
+        for i = 0 to m - 1 do
+            trace <- trace + this.[i,i]
+        trace
+
+    static member (+) (a : Matrix, b : Matrix) =
+        assert (a.N = b.N && a.M = b.M)
+        let c = Matrix(a.M, a.N)
+        for i = 0 to a.M - 1 do
+            for j = 0 to a.N - 1 do
+                c.[i,j] <- a.[i,j] + b.[i,j]
+        c
+    static member (-) (a : Matrix, b : Matrix) =
+        assert (a.N = b.N && a.M = b.M)
+        let c = Matrix(a.M, a.N)
+        for i = 0 to a.M - 1 do
+            for j = 0 to a.N - 1 do
+                c.[i,j] <- a.[i,j] - b.[i,j]
+        c
+    // NOT the most efficient, but we should only be dealing with 4x4 matrices at most
+    static member (*) (a : Matrix, b : Matrix) = 
+        assert(a.N = b.M)
+        let newMat = Matrix(a.M,b.N)
+        for i = 0 to a.M - 1 do
+            for j = 0 to b.N - 1 do
+                let mutable sum = 0.0
+                for k = 0 to a.N - 1 do
+                    sum <- sum + a.[i,k] * b.[k,j]
+                newMat.[i,j] <- sum
+        newMat
+    static member Identity(n) =
+        let newMat = Matrix(n, n)
+        for i = 0 to n - 1 do
+            newMat.[i,i] <- 1.0
+        newMat
+    override this.ToString() =
+        let mutable ret = ""
+        ret <- ret + "{\n"
+        for i = 0 to m - 1 do
+            ret <- ret + "\t"
+            for j = 0 to n - 1 do
+                ret <- ret + this.[i,j].ToString() + "\t"
+            ret <- ret + "\n"
+        ret <- ret + "}\n"
+        ret
+
+type Matrix3() =
+    inherit Matrix(3,3)
+
+    member this.Determinant() =
+        this.[0,0] * (this.[1,1] * this.[2,2] - this.[1,2] * this.[2,1]) +
+        this.[0,1] * - (this.[1,0] * this.[2,2] - this.[1,2] * this.[0,2]) +
+        this.[0,2] * (this.[0,1] * this.[1,2] - this.[1,1] * this.[0,2])
+    member this.Inverse() =
+        let det = this.Determinant()
+        assert(det <> 0.0)
+        let detInv = 1.0 / det
+        let tr = this.Trace()
+        let msqr = this * this
+        let tr2 = msqr.Trace()
+        do printf "%f\n%f\n" det tr
+        // Hard coded Cayley-Hamilton Method
+        (Matrix.Identity(3).ScalarMult(0.5 * (tr * tr - tr2)) - this.ScalarMult(tr) + msqr).ScalarMult(detInv)
+
+                           
+
+type Matrix4() =
+    inherit Matrix(4,4)
+
+    member this.Minor3() = 
+        let newMat = Matrix3()
+        for i = 0 to 2 do
+            for j = 0 to 2 do
+                newMat.[i,j] <- this.[i,j]
+        newMat
 
 // TODO: implement a transform class for using with rendered entities
+type Transform() =
+    let mutable position = Vec3D()
+    let mutable rotation = Vec3D()
+    let mutable scale = Vec3D()
+    // TODO
 
 // TODO: abstract a little further and add a List<Vec2D> for storing all the vertices of the shape
 [<AbstractClass>]
